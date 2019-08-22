dfb65efb81c836e368224e2046ac81cb354f638e
zhangchongcai <18207754940@163.com>
2019-08-22 15:11:58 +0800

    视图

--- server/server.js
+++ server/server.js
@@ -16,14 +16,8 @@ app.use('/public',express.static(path.join(__dirname,'./public')))
 //引用内置模块
 const users  = require('./router/users.js')
 const colulist = require("./router/coluList.js")
-const newProducts = require('./router/goodslist/newProducts.js')
-const importProducts = require('./router/goodslist/importProducts.js')
-const importDetail = require('./router/goodslist/importDetail.js')
 const cart = require('./router/cart.js')
-const address = require('./router/address.js')
-const typeList = require('./router/sortGoods.js')
 const swiper = require('./router/swiper.js')
-const shenqian = require('./router/goodslist/shenQianList.js')
 
 
 
@@ -32,13 +26,7 @@ const shenqian = require('./router/goodslist/shenQianList.js')
 //路由
 app.use('/api/users',users) 
 app.use('/api/coluList',colulist)
-app.use('/api/newProducts',newProducts)
-app.use('/api/importProducts',importProducts)
-app.use('/api/importDetail',importDetail)
-app.use('/api/shenqian',shenqian)
 app.use('/api/cart',cart)
-app.use('/api/address',address)
-app.use('/api/typeList',typeList)
 app.use('/api/swiper',swiper)
 
 
